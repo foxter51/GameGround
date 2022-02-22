d4b2349af41e6510f8888c0237543fdbbf41e143
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2022-02-22 13:44:41 +0200

    language support

---
+++ src/main/resources/static/js/switch.js
@@ -0,0 +1,104 @@
+$(function (){
+    $("#modeCheck").change(function (){
+        if($('#modeCheck').prop('checked')) {
+            localStorage.setItem('darkMode', 'true');
+            setDarkMode();
+            transitionAfterChange()
+        } else{
+            localStorage.setItem('darkMode', 'false');
+            removeDarkMode();
+            transitionAfterChange()
+        }
+    });
+    $("#languageCheck").change(function (){
+        if($('#languageCheck').prop('checked')) {
+            localStorage.setItem('lang', 'true');
+            setRussian();
+        } else{
+            localStorage.setItem('lang', 'false');
+            setEnglish();
+        }
+    });
+})
+
+window.addEventListener('load', function(){
+    if (localStorage.getItem('darkMode')==='true'){
+        setDarkMode();
+        $("#modeCheck").prop('checked', true);
+    } else{
+        removeDarkMode();
+        $("#modeCheck").prop('checked', false);
+    }
+    if(localStorage.getItem('lang')==='true'){
+        setRussian();
+        $("#languageCheck").prop('checked', true);
+    } else{
+        setEnglish();
+        $("#languageCheck").prop('false');
+    }
+});
+
+function setDarkMode(){
+    $('body').addClass('bg-dark text-white');
+    $(".card").addClass('bg-dark text-white border border-white');
+}
+
+function removeDarkMode(){
+    $('body').removeClass('bg-dark text-white');
+    $(".card").removeClass('bg-dark text-white border border-white');
+}
+
+function transitionAfterChange(){
+    $("#viewProfile").css("transition", '1s');
+    $(".card").css("transition", '1s');
+}
+
+function setRussian(){
+    $('[name="cardCol"]').removeClass("col-sm-3").addClass("col-sm-4");
+    $("#homeLabel").text('Главная');
+    $("#regFormLabel").text('Форма регистрации');
+    $("#authFirst").text('Имя');
+    $("#authLast").text('Фамилия');
+    $("#authPass").text('Пароль');
+    $("#regButton").text('Регистрация');
+    $("#orLabel").text('ИЛИ');
+    $("#logFormLabel").text('Форма входа');
+    $("#badLogLabel").text('Неверные данные или Вы были заблокированы!');
+    $("#logButton").text('Войти');
+    $("#logButtonLabel").text('Вход');
+    $("#regButtonLabel").text('Регистрация');
+    $("#roleLabel").text('Редактор');
+    $("#nameLabel").text('Полное имя');
+    $("#regLabel").text('Регистрация');
+    $("#lastLoginLabel").text('Посл. вход');
+    $("#darkModeLabel").text('Темная тема');
+    $("#checkOptMode").text('Вкл.');
+    $("#langLabel").text('Язык');
+    $("#checkOptLang").text('Русский');
+    $("#footerLabel").text('Все права защищены. 2022');
+}
+
+function setEnglish(){
+    $('[name="cardCol"]').removeClass("col-sm-4").addClass("col-sm-3");
+    $("#homeLabel").text('Home');
+    $("#regFormLabel").text('Registration form');
+    $("#authFirst").text('First name');
+    $("#authLast").text('Last name');
+    $("#authPass").text('Password');
+    $("#regButton").text('Register');
+    $("#orLabel").text('OR');
+    $("#logFormLabel").text('Login form');
+    $("#badLogLabel").text('Bad credentials or you have been blocked!');
+    $("#logButton").text('Login');
+    $("#logButtonLabel").text('Login');
+    $("#regButtonLabel").text('Register');
+    $("#roleLabel").text('Editor');
+    $("#nameLabel").text('Full name');
+    $("#regLabel").text('Registered');
+    $("#lastLoginLabel").text('Last login');
+    $("#darkModeLabel").text('Dark mode');
+    $("#checkOptMode").text('On');
+    $("#langLabel").text('Language');
+    $("#checkOptLang").text('Russian');
+    $("#footerLabel").text('All rights are reserved. 2022');
+}
