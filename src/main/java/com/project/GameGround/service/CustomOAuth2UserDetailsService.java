63a4dbc7d7c674e297d6b1ae56919a0268a91fb7
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2022-03-14 20:32:01 +0200

    fixes

--- src/main/java/com/project/GameGround/service/CustomOAuth2UserDetailsService.java
+++ src/main/java/com/project/GameGround/service/CustomOAuth2UserDetailsService.java
@@ -28,7 +28,7 @@ public class CustomOAuth2UserDetailsService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
         OAuth2User OAuthUser = super.loadUser(userRequest);
         User user = repo.findByEmail(OAuthUser.getAttribute("email"));  //searching for user in database
-        return new CustomOAuth2UserDetails(OAuthUser, user);  //returns new user after successful auth
+        return null;  //returns new user after successful auth
     }
 
     public void createUserAfterOAuth(String email, String name, AuthProvider provider){
