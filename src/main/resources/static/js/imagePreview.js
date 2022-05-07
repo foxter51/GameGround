81bc544ae26e00e505a5d40f1b2798f2b559fab5
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2022-05-07 19:08:37 +0300

    fixes

--- src/main/resources/static/js/imagePreview.js
+++ src/main/resources/static/js/imagePreview.js
@@ -1,15 +1,25 @@
 $(document).ready(()=>{
-    let currentHref = $(location).attr('href');
+    let file;
+    let reader = new FileReader();
+
     $('#previewPhotoCreate').hide();
-    $('#photoUpload').change(function(){
-        const file = this.files[0];
+
+    $('#photoUploadCreate').change(function(){
+        file = this.files[0];
         if (file){
-            let reader = new FileReader();
             reader.onload = function(event){
-                if(currentHref.includes('profile')) {$('#previewPhotoCreate').show().attr('src', event.target.result);}
-                else $('#previewPhoto').attr('src', event.target.result);
+                $('#previewPhotoCreate').show().attr('src', event.target.result);
             }
-            reader.readAsDataURL(file);
         }
+        reader.readAsDataURL(file);
+    });
+    $('#photoUploadEdit').change(function (){
+        file = this.files[0];
+        if (file){
+            reader.onload = function(event){
+                $('#previewPhotoEdit').attr('src', event.target.result);
+            }
+        }
+        reader.readAsDataURL(file);
     });
 });
